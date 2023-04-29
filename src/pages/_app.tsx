a906c96c1df4ec07bcfb5da5010956b4102f37d1
Ioannis Tziligkakis <gtsiligkakis@gmail.com>
2023-04-29 16:15:31 +0300

    Setup react-query client provider and hydration component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,31 @@
 import "@/styles/globals.css";
+import { Hydrate, QueryClient, QueryClientProvider } from "react-query";
 import type { AppProps } from "next/app";
+import { useState } from "react";
+
+const QUERY_RETRY_COUNT = 3;
+const QUERY_CACHE_TTL = 15 * 60 * 1000;
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            keepPreviousData: true,
+            refetchOnMount: false,
+            retry: QUERY_RETRY_COUNT,
+            staleTime: QUERY_CACHE_TTL,
+          },
+        },
+      })
+  );
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Hydrate state={pageProps.dehydratedState}>
+        <Component {...pageProps} />
+      </Hydrate>
+    </QueryClientProvider>
+  );
 }
